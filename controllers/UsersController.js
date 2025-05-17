79f2204148f16c92cd8eb3a4cd102d4767b6387e
Miguel Gomez <bydemon101@gmail.com>
2025-05-16 23:13:45 -0500

    Eliminacion Usuario

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,18 @@
 const User = require('../models/User');
+
+
+// Eliminar usuario
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        
+        if (!user) {
+            return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+        }
+        
+        res.json({ mensaje: 'Usuario eliminado correctamente' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al eliminar usuario' });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/UsersController');
 
-
+// Ruta para eliminar un usuario
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
